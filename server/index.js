9870ce430cbdf34ecc73aeeaf9910b47011a39c4
Matthew Crawford <matthewcrawford@Matthews-MacBook-Air.local>
2021-11-03 18:17:16 -0700

    Created route to fetch xml data, parse it and return object with desired values. Displayed information on web page.

--- server/index.js
+++ server/index.js
@@ -1,10 +1,55 @@
+const { default: axios } = require('axios');
 const express = require('express');
+const xml2js = require('xml2js');
+const parser = new xml2js.Parser();
 
 const app = express();
 
+const PORT = 3000 || process.env.PORT;
+
 app.use(express.static('public'));
 
-const PORT = 3000 || process.env.PORT;
+app.get('/api/housingData', (req, res) => {
+    var properties = [];
+    axios.get('https://s3.amazonaws.com/abodo-misc/sample_abodo_feed.xml')
+    .then(({data}) => {
+        // var parseString = require('xml2js').parseString;
+        parser.parseString(data, function (err, result) {
+            // console.log('result: ', result.PhysicalProperty);
+            // console.log('result down to property: ', result.PhysicalProperty.Property);
+            // console.log('result down to property ID: ', result.PhysicalProperty.Property[0].PropertyID[0].Email[0]);
+            // console.log('result down to address: ', result.PhysicalProperty.Property[0].PropertyID[0].Address[0].City);
+            console.log('result down to bedrooms: ', result.PhysicalProperty.Property[0].ILS_Unit[0].Units[0].Unit[0].UnitBedrooms[0]);
+
+            var propertyList = result.PhysicalProperty.Property;
+            propertyList.map((property) => {
+                if (property.PropertyID[0].Address[0].City[0] === 'Madison' && property.PropertyID[0].Address[0].State[0] === 'WI') {
+                    var info = {
+                        property_id: property.PropertyID[0].Identification[0]['$'].IDValue,
+                        name: property.PropertyID[0].MarketingName[0],
+                        email: property.PropertyID[0].Email[0],
+                        bedrooms: property.ILS_Unit[0].Units[0].Unit[0].UnitBedrooms[0]
+                    };
+                    properties.push(info)
+                }
+                // var indProperty = {};
+                // dataToSend.push(property[0].PropertyID[0].Address[0].City[0])
+            })
+            // console.log('dataToSend: ', dataToSend)
+            res.send(properties)
+        });
+        // iterate over result.PhysicalProperty.Property (array of all the properties)
+            // for each property
+
+        // console.log('response: ', response)
+        // res.send(response)
+        // data wrangling etc.
+        // eventually send the data back in a nice format
+    })
+    .catch(err => {
+        res.sendStatus(404)
+    })
+})
 
 app.listen(PORT, () => {
     console.log(`App running on localhost:${PORT}`)
--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+    const [properties, setProperties] = React.useState([]);
+    useEffect(() => {
+        getData()
+    }, []);
+    const getData = () => {
+        axios.get('/api/housingData')
+        .then(({data}) => {
+            setProperties(data)
+        })
+        .catch((err) => {
+            console.log('err: ', err)
+        })
+    }
     return (
         <div>
-            React Working
+            <div>
+                {properties.map((property, index) => {
+                    return (
+                        <div key={index}>
+                            <div>property_id: {property.property_id}</div>
+                            <div>name: {property.name}</div>
+                            <div>email: {property.email}</div>
+                            <div>bedrooms: {property.bedrooms}</div>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
